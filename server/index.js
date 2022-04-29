05395cefc10d22e02618b26fdede44c09e7f00d4
Jameson Chen <[jamesonychan@gmail.com]>
2022-04-29 02:56:16 -0400

    fixed .env not loading correctly commit

--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
 
 // middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -15,12 +18,10 @@ app.use(cors());
 app.use("/api/posts", postRoutes);
 
 // mongodb config
-const CNNECTION_URL =
-  "mongodb+srv://memories:6SfsEEFNJu3shq0T@cluster0.mfkcg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CNNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
